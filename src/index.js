0e315e231831ec1efd78892193d5b75eddfd87a9
David Hérault <dherault@gmail.com>
2019-04-19 12:16:15 +0200

    edit js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@ const { width, height } = canvas
 
 const _ = canvas.getContext('2d')
 
-function drawSquareAndQuarterCircle(size, angle) {
-  _.save()
-  _.rotate(angle)
-
+function drawSquareAndQuarterCircle(size) {
   _.beginPath()
   _.rect(0, 0, size, size)
   _.arc(0, 0, size, 0, Math.PI / 2)
   _.stroke()
-  _.restore()
 }
 
 let size = 10
@@ -28,7 +24,7 @@ _.translate(width / 2, height / 2)
 // _.rotate(-Math.PI / 2)
 
 for (let i = 0; i < 15; i++) {
-  drawSquareAndQuarterCircle(size, 0)
+  drawSquareAndQuarterCircle(size)
 
   const tempSize = previousSize
   previousSize = size
